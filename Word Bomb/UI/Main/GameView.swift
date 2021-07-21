2e05d58298803964a06490741c6ffe9e8ed46b2d
beetee17 <brandon.thio@yahoo.com.sg>
2021-07-21 16:12:53 +0800

    edit help sheet

--- Word Bomb/UI/Main/GameView.swift
+++ Word Bomb/UI/Main/GameView.swift
@@ -32,7 +32,7 @@ struct GameView: View {
                 GamePlayView()
                 
                 PauseMenuView()
-                    .scaleEffect(.pauseMenu == viewModel.viewToShow ? 1 : 0)
+                    .scaleEffect(.pauseMenu == viewModel.viewToShow ? 1 : 0.01)
             }
             
         }
