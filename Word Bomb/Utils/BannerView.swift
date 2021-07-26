f1ddfe21230d19c2886471acb8ce752aeae4db1e
beetee17 <brandon.thio@yahoo.com.sg>
2021-07-26 13:34:08 +0800

    fix banner transition not animating on disappear due to zIndex

--- Word Bomb/Utils/BannerView.swift
+++ Word Bomb/Utils/BannerView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct BannerView: View {
     let title: String
     let message: String
-
+    
     var body: some View {
         ZStack {
             ZStack {
                 LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.03205184274, green: 0.03314970656, blue: 0.04729758212, alpha: 1)), Color("Background")]), startPoint: .top, endPoint: .bottom)
-                        
+                
                 LinearGradient(gradient: Gradient(colors: [Color("Background").opacity(0.6), Color(#colorLiteral(red: 0.07820445112, green: 0.08963582299, blue: 0.09931479403, alpha: 1)).opacity(0.3)]), startPoint: .top, endPoint: .bottom)
-            
+                
                 Color.black.opacity(0.2)
-
+                
             }
             .frame(width: Device.width-40, height: 100, alignment: .leading)
             .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
@@ -42,39 +42,42 @@ struct BannerView: View {
                         .bold()
                     
                     Text(message)
-                        
+                    
                 }
             }
             .padding(.horizontal, 5)
             .frame(width: Device.width-40, height: 75, alignment: .leading)
         }
-        .animation(.easeInOut)
-        .transition(AnyTransition.asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom)).combined(with: .opacity))
+        
     }
 }
 struct BannerViewModifier: ViewModifier {
     @Binding var isPresented: Bool
     let title: String
     let message: String
-
+    
     func body(content: Content) -> some View {
         ZStack {
             content
-        
-        VStack(spacing: 0) {
             if isPresented {
-                BannerView(title: title, message: message)
-                    .onAppear() {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            withAnimation(.easeInOut) { self.isPresented = false }
+                VStack(spacing: 0) {
+                    
+                    BannerView(title: title, message: message)
+                        .onAppear() {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                withAnimation { self.isPresented = false }
+                            }
                         }
-                    }
+                    Spacer()
+                }
+                .transition(.move(edge: .top).combined(with: .fade))
+                .animation(.easeInOut(duration: 0.5))
+                .zIndex(1)
             }
-            Spacer()
-        }
         }
     }
 }
+
 
 extension View {
     func banner(isPresented: Binding<Bool>, title: String, message: String) -> some View {
